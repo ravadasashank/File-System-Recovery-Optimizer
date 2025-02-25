2c23ce32d4e1ec57d6a1d5e8da97e172ec4b4607
ravadasashank <ravadasashank444@gmail.com>
2025-02-25 21:44:30 +0530

    Implemented free-space management using bitmaps

--- src/file_system.py
+++ src/file_system.py
@@ -29,3 +29,27 @@ fs = FileSystem("virtual_fs")
 fs.create_file("test.txt", "Hello World!")
 fs.list_files()
 fs.delete_file("test.txt")
+
+class FreeSpaceManager:
+    def __init__(self, total_blocks):
+        self.total_blocks = total_blocks
+        self.bitmap = [0] * total_blocks  # 0 = free, 1 = occupied
+
+    def allocate_block(self):
+        for i in range(self.total_blocks):
+            if self.bitmap[i] == 0:
+                self.bitmap[i] = 1
+                print(f"Allocated block {i}")
+                return i
+        print("No free blocks available")
+        return -1
+
+    def free_block(self, block_number):
+        if 0 <= block_number < self.total_blocks:
+            self.bitmap[block_number] = 0
+            print(f"Block {block_number} freed")
+
+# Example Usage
+fsm = FreeSpaceManager(10)
+fsm.allocate_block()
+fsm.free_block(0)
