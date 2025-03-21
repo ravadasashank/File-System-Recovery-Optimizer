47f34f9dbb667c285a23d0f8c9a63bb132bc05cd
ravadasashank <ravadasashank444@gmail.com>
2025-03-21 22:15:00 +0530

    Added visualization for disk usage

--- src/optimization.py
+++ src/optimization.py
@@ -21,3 +21,18 @@ cache.access_file("file1.txt")
 cache.access_file("file2.txt")
 cache.access_file("file3.txt")
 cache.access_file("file4.txt")  # This will remove the oldest file (file1.txt)
+
+import matplotlib.pyplot as plt
+
+def visualize_disk_usage(used_blocks, total_blocks):
+    labels = ["Used Space", "Free Space"]
+    sizes = [used_blocks, total_blocks - used_blocks]
+    colors = ["red", "green"]
+
+    plt.figure(figsize=(6,6))
+    plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
+    plt.title("Disk Usage")
+    plt.show()
+
+# Example Usage
+visualize_disk_usage(30, 100)  # 30% used, 70% free
